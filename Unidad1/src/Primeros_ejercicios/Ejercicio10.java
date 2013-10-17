4cf19495c99ca7a5be5808dec0451362efe8d202
dgarciag87 <danygarcia1322@gmail.com>
2013-10-17 17:39:51 +0200

    10 corregido

--- Unidad1/src/Primeros_ejercicios/Ejercicio10.java
+++ Unidad1/src/Primeros_ejercicios/Ejercicio10.java
@@ -1,17 +1,38 @@
 package Primeros_ejercicios;
 
-import java.util.Scanner;
-
 public class Ejercicio10 {
 	public static void main(String[] args) {
 		float x,y,z;
 		final float PI=3.14f;
-		boolean a;
-		float b,c,d,e;
+		boolean a,b,c,d,e,f;
 		
 		a=((4-2)*(5+1)/2)>2-(4+3);
 		System.out.print("\nEl resultado es: "+a);
 		
+		b=(6+3)>8 && (6-1)*2 < 8 || 23==8;
+		System.out.print("\nEl resultado es: "+b);
 		
+		x=7;
+		z=2;
+		c=(1.0 < x) && (x < z+7.0);
+		System.out.print("\nEl resultado es: "+c);
+			
+		x=1;
+		y=4;
+		z=10;
+		d=(PI * x*x > y || 2*PI*x <= z);
+		System.out.print("\nEl resultado es: "+d);
+				
+		x=1;
+		y=4;
+		z=10;
+		e=(x>3 && y==4 || x + y <= z);
+		System.out.print("\nEl resultado es: "+e);
+			
+		x=1;
+		y=4;
+		z=10;
+		f=(x>3 && (y==4 || x + y <= z));
+		System.out.print("\nEl resultado es: "+f);		
 	}
 }
